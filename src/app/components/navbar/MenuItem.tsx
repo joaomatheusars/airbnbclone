82cfed5f87d100a0df55b47748f527ab0305626a
joaomatheusars <joaomatheusars@gmail.com>
2023-08-09 12:11:29 -0300

    fit: add UI register

---
+++ src/app/components/navbar/MenuItem.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+interface MenuItemProp {
+    onClick: () => void;
+    label: string;
+}
+const MenuItem: React.FC<MenuItemProp> = ({
+    onClick,
+    label
+}) => {
+    return ( 
+        <div onClick={onClick} className="px-4 py-3 hover:bg-neutral-100 transition font-semibold">
+            {label}
+        </div>
+    );
+}
+ 
+export default MenuItem;
