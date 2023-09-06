9fcc0903fb97d313ab12107598bd42070e3843ca
João Matheus Albergaria Rodrigues Santana <51250239+joaomatheusars@users.noreply.github.com>
2023-09-06 07:45:12 -0300

    fit: listing server components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,46 @@
-export default function Home() {
+import getCurrentUser from "./actions/getCurrentUser";
+import getListings from "./actions/getListings";
+import ClientOnly from "./components/ClientOnly"
+import Container from "./components/Container"
+import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listinigs/ListingCard";
+
+export default async function Home() {
+  const listings = await getListings();
+  const currentuser = await getCurrentUser();
+
+  if (listings.length === 0 ){
+    return(
+      <ClientOnly>
+        <EmptyState showRest/>
+      </ClientOnly>
+    )
+  }
   return (
-    <div>
-    </div>
+    <ClientOnly>
+      <Container>
+        <div className="
+          pt-24
+          grid
+          grid-cols-1
+          sm:grid-cols-2
+          md:grid-cols-3
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+          gap-8
+        ">
+          {listings.map((listings: any) =>{
+            return(
+              <ListingCard
+                currentuser={currentuser}
+                key={listings.id}
+                data={listings}
+              />
+            )
+          })}
+        </div>
+      </Container>
+    </ClientOnly>
   )
 }
---
+++ src/app/providers/ToasterProvider.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { Toaster } from 'react-hot-toast';
+
+const ToaterProvider = () => {
+    return (
+        <Toaster />
+    );
+}
+ 
+export default ToaterProvider;
